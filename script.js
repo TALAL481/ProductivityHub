17d54c59da79a0e09fdd50cd595d3f26db3f22fb
TALAL481 <drazra_22@hotmail.com>
2026-07-30 17:12:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const search = document.getElementById("search");
 
+const tools = document.querySelectorAll(".tool");
+
+search.addEventListener("input", () => {
+
+  const value = search.value.toLowerCase();
+
+  tools.forEach(tool => {
+
+    const text = tool.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+
+      tool.style.display = "block";
+
+    } else {
+
+      tool.style.display = "none";
+
+    }
+
+  });
+
+});
